7eb8d759f959362c0ceebafa52a7843ceffb0cfe
scliu99 <scliu99@ntub.edu.tw>
2025-08-11 11:29:17 +0800

    Update ch1_04.c
    修正多出來的符號

--- ch01/ch1_04.c
+++ ch01/ch1_04.c
@@ -18,7 +18,7 @@ int main(void)
    p[0].score = 75;             /* 將p[0]學生的成績指定為75 */
    strcpy(p[1].id,"S9903502");  /* 將p[1]學生的學號指定為字串S9903502 */
    p[1].score = 53;             /* 將p[1]學生的成績指定為53 */
-      :
+      
    for(Sum=0,i=0;i<num;i++)
        Sum = Sum + p[i].score;      /*  累加學生成績 */
    Avg = Sum / num;
